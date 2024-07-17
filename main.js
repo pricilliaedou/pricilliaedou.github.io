c45f39eac1dd6ae4cbc0624d75b1c7ee77bd7a20
pricillia <pmoussavou@gmail.com>
2024-07-17 12:41:21 +0200

    header

---
+++ main.js
@@ -0,0 +1,6 @@
+var hamburger = document.getElementById("hamburger");
+// var container = document.getElementById("container");
+hamburger.addEventListener("click", open);
+function open() {
+  navlink.classList.toggle("open");
+}
